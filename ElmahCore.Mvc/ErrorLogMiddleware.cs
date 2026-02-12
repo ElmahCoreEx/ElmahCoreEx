0fdb5c5cb3d64c2fae5b70deb4dd6b306c89c71e
codefactor-io <support@codefactor.io>
2026-02-12 07:05:50 +0000

    [CodeFactor] Apply fixes

--- ElmahCore.Mvc/ErrorLogMiddleware.cs
+++ ElmahCore.Mvc/ErrorLogMiddleware.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -272,8 +272,7 @@ internal sealed class ErrorLogMiddleware
     internal async Task<string> LogException(Exception e, HttpContext context,
         Func<HttpContext, Error, Task> onError, string body = null, int? statusCode = null)
     {
-        if (e == null)
-            throw new ArgumentNullException(nameof(e));
+        ArgumentNullException.ThrowIfNull(e);
 
         //
         // Fire an event to check if listeners want to filter out
@@ -285,7 +284,7 @@ internal sealed class ErrorLogMiddleware
         try
         {
             var args = new ExceptionFilterEventArgs(e, context);
-            if (_filters.Any())
+            if (_filters.Count != 0)
             {
                 OnFiltering(args);
 
